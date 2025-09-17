afd38e50f2b328d96965b0c95146a8cec499ce97
I21403 <I21403@lab.aca>
2025-09-17 12:31:24 -0500

    Usando boostrap

---
+++ src/FormularioLogIn.tsx
@@ -0,0 +1,14 @@
+const FormularioLogIn = ()=>{
+    return <form>
+    <div>
+      <label>Usuario:</label>
+      <input type="text"/>
+    </div>
+    <div>
+      <label>Password:</label>
+      <input type="text"/>
+    </div>
+    <button type='button'>Ingresar</button>
+  </form>
+}
+export default FormularioLogIn
---
+++ src/Titulo.tsx
@@ -0,0 +1,5 @@
+const Titulo = () =>{
+    return <h1>Login</h1>
+}
+
+export default Titulo
